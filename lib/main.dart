28c5124b3cddb1bd14a8672152de3afb158f75cb
Cullex <54881998+Cullex@users.noreply.github.com>
2023-03-28 16:00:57 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,77 +1,38 @@
-import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:flutter/material.dart';
 
-const _kPages = <String, IconData>{
-  'home': Icons.home,
-  'map': Icons.map,
-  'add': Icons.add,
-  'message': Icons.message,
-  'people': Icons.people,
-};
-
-class ConvexAppExample extends StatefulWidget {
-  const ConvexAppExample({super.key});
-
-  @override
-  _ConvexAppExampleState createState() => _ConvexAppExampleState();
-}
-
-class _ConvexAppExampleState extends State<ConvexAppExample> {
-  TabStyle _tabStyle = TabStyle.reactCircle;
+class TabsExample extends StatelessWidget {
+  const TabsExample({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final _kTabPages = <Widget>[
+      const Center(child: Icon(Icons.cloud, size: 64.0, color: Colors.teal)),
+      const Center(child: Icon(Icons.alarm, size: 64.0, color: Colors.cyan)),
+      const Center(child: Icon(Icons.forum, size: 64.0, color: Colors.blue)),
+    ];
+    final _kTabs = <Tab>[
+      const Tab(icon: Icon(Icons.cloud), text: 'Tab1'),
+      const Tab(icon: Icon(Icons.alarm), text: 'Tab2'),
+      const Tab(icon: Icon(Icons.forum), text: 'Tab3'),
+    ];
     return DefaultTabController(
-      length: 5,
-      initialIndex: 2,
+      length: _kTabs.length,
       child: Scaffold(
-        body: Column(
-          children: [
-            _buildStyleSelector(),
-            const Divider(),
-            Expanded(
-              child: TabBarView(
-                children: [
-                  for (final icon in _kPages.values) Icon(icon, size: 64),
-                ],
-              ),
-            ),
-          ],
+        appBar: AppBar(
+          title: const Text('appbar title'),
+          backgroundColor: Colors.cyan,
+          // If `TabController controller` is not provided, then a
+          // DefaultTabController ancestor must be provided instead.
+          // Another way is to use a self-defined controller, c.f. "Bottom tab
+          // bar" example.
+          bottom: TabBar(
+            tabs: _kTabs,
+          ),
         ),
-        bottomNavigationBar: ConvexAppBar.badge(
-          // Optional badge argument: keys are tab indices, values can be
-          // String, IconData, Color or Widget.
-          /*badge=*/ const <int, dynamic>{3: '99+'},
-          style: _tabStyle,
-          items: <TabItem>[
-            for (final entry in _kPages.entries)
-              TabItem(icon: entry.value, title: entry.key),
-          ],
-          onTap: (int i) => print('click index=$i'),
+        body: TabBarView(
+          children: _kTabPages,
         ),
       ),
-    );
-  }
-
-  // Select style enum from dropdown menu:
-  Widget _buildStyleSelector() {
-    final dropdown = DropdownButton<TabStyle>(
-      value: _tabStyle,
-      onChanged: (newStyle) {
-        if (newStyle != null) setState(() => _tabStyle = newStyle);
-      },
-      items: [
-        for (final style in TabStyle.values)
-          DropdownMenuItem(
-            value: style,
-            child: Text(style.toString()),
-          )
-      ],
-    );
-    return ListTile(
-      contentPadding: const EdgeInsets.all(8),
-      title: const Text('appbar style:'),
-      trailing: dropdown,
     );
   }
 }
